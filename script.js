67246af36864eac16d9aa7566b6ec4ce4041f4a2
universaldeveloper-com <amemus490@gmail.com>
2025-12-12 03:32:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ const cardsData = [
         id: 1,
         chapterName: "THE BEGINNING",
         image: "pic1.jpg", 
-        secretMessage: "hmm tho chaoter 1 complete krdiya huh thik he achi ladki he th par bauni he acha tho me ye bolne wala hu ki u look gorgeous in thst green dress boyfriend tho he nai kehne keliye tho yaha bol rahahu i mean im now half blood prince par fir bhi tu bhuudi he 😏 acha chal chal kabi nai kiya hu par ab kar rahahu aur nai karynga agar mere vaja se kabi uncomfortable, awkward, strange , irritating, eww type feel hua tho  maf kardo 🙏🏻 aur boldena kabi nai karunga vo kya hena ki muje thoda attachment issues he koi care karta he tho unko aur bhi arene lagta hu chal chal bhuddi ab zyada mat soch tereko kuch aur kehna he tho mene soch liya ki tuje disney land le chalu kabhi ab zyada mat soch lechelunga mat ro pur tu manna padega ki tu bhuddi aur ganji he 😏 tab hi future ka ticket confirm hoga  aree bhul hi gaya  most imp thing is  ", 
+        secretMessage: " hmm tho chaoter 1 complete krdiya huh thik he achi ladki he th par bauni he acha tho me ye bolne wala hu ki u look gorgeous in thst green dress boyfriend tho he nai kehne keliye tho yaha bol rahahu i mean im now half blood prince par fir bhi tu bhuudi he 😏 acha chal chal kabi nai kiya hu par ab kar rahahu aur nai karynga agar mere vaja se kabi uncomfortable, awkward, strange , irritating, eww type feel hua tho  maf kardo 🙏🏻 aur boldena kabi nai karunga vo kya hena ki muje thoda attachment issues he koi care karta he tho unko aur bhi arene lagta hu chal chal bhuddi ab zyada mat soch tereko kuch aur kehna he tho mene soch liya ki tuje disney land le chalu kabhi ab zyada mat soch lechelunga mat ro pur tu manna padega ki tu bhuddi aur ganji he 😏 tab hi future ka ticket confirm hoga  aree bhul hi gaya  most imp thing is", 
         password: "sehjisqueen", 
         locked: true,
-        // NEW SETTINGS FOR THE PRANK
+        // PRANK SETTINGS
         requiresAgreement: true,
         agreementText: "I agree sam is always best",
-        agreed: false // Don't change this
+        agreed: false
     },
     {
         id: 2,
         chapterName: "GOLDEN BROWNS",
         image: "", 
-        secretMessage: "CONGRATULATIONS tune chapter 2 bhi complete kiya he hmm bahut kuch batana he bahut kuch khul ke batana he par kuch bate keh nai sakta shej,hum pata nai kis reason ki waja se mile he par u r the lucky charam and good happened this year ngl bauni he par badi achi he yar ek bat batau muje pata nai ye friendship kab tak rahega par jab tak hoga tab tak i care about u, i adore you, i help you, i motivate you . kabhi socha nai ki ek 5 feet ki kubsurat bauni se itna jaldi attach hojaunga. bus kehta nai hu par i miss you a lot  ahmmm ahmmm kuch emotional hgya hu chal chal bandr kahiki sigma se soft bana rahihe . kabhi zindagi me muskil aya aur tuje help chahiye tho muje yad rakna socha ta ki breakup ke bad koi aur ldki se bat tak nsi karunga par kher kon bola tu ladki he . tu health ka dyan rako aur tention mat kr carrier ka kuch ameer ladko se shadi krlo aur paise dedo muje chill krunga mst hahaha. tq 4 everything seno. even tho we know each other less than 3 months but feel like i know u from 3 years. abe sun alooo khayega? hehe. i wish i know u before this is not just a sentence. ab jobi ho tera problems is mera problems so phadle chudail tu sachi me chapters complete kr rahihona 🤨 bhut barunga nai nai marsakta par bech dunga . sehj , i really respect you ( tatti ka respect). i dont make promises because ik i break them but i promise you one day we will explore this world . are kich kehna he bul gaya han asli chez tho bolna hi bhul gaya this one may stunn u nd u may have many questions u even cry so i ",
+        secretMessage: " CONGRATULATIONS tune chapter 2 bhi complete kiya he hmm bahut kuch batana he bahut kuch khul ke batana he par kuch bate keh nai sakta shej,hum pata nai kis reason ki waja se mile he par u r the lucky charam and good happened this year ngl bauni he par badi achi he yar ek bat batau muje pata nai ye friendship kab tak rahega par jab tak hoga tab tak i care about u, i adore you, i help you, i motivate you . kabhi socha nai ki ek 5 feet ki kubsurat bauni se itna jaldi attach hojaunga. bus kehta nai hu par i miss you a lot  ahmmm ahmmm kuch emotional hgya hu chal chal bandr kahiki sigma se soft bana rahihe . kabhi zindagi me muskil aya aur tuje help chahiye tho muje yad rakna socha ta ki breakup ke bad koi aur ldki se bat tak nsi karunga par kher kon bola tu ladki he . tu health ka dyan rako aur tention mat kr carrier ka kuch ameer ladko se shadi krlo aur paise dedo muje chill krunga mst hahaha. tq 4 everything seno. even tho we know each other less than 3 months but feel like i know u from 3 years. abe sun alooo khayega? hehe. i wish i know u before this is not just a sentence. ab jobi ho tera problems is mera problems so phadle chudail tu sachi me chapters complete kr rahihona 🤨 bhut barunga nai nai marsakta par bech dunga . sehj , i really respect you ( tatti ka respect). i dont make promises because ik i break them but i promise you one day we will explore this world . are kich kehna he bul gaya han asli chez tho bolna hi bhul gaya this one may stunn u nd u may have many questions u even cry so i ",
         password: "samisbatman", 
         locked: true,
-        requiresAgreement: false, // No prank for this one
+        requiresAgreement: false,
         agreed: true
     },
     {
         id: 3,
-        chapterName: "TO ME,ITS HEAVEN",
+        chapterName: "IT IS,HEAVEN FOR ME",
         image: "",
         secretMessage: "LOVE",
         password: "03062007", 
@@ -37,10 +37,10 @@ const cardsData = [
     },
     {
         id: 4,
-        chapterName: "PROVE ME WRONG",
+        chapterName: "PROVE ME WRONG ",
         image: "",
-        secretMessage: "you!, yes u heared it right i love you , respectfully.sehj meri jindagi me 2 hi ladkiya ayi he ab tak ek vo jis ke bare me bat nai karunga aur ek tum bat kare tho karta hi rahunga ab ye mat soch na ki line mar rahahu ,maybe oh nono not nai mar rahahu . tho last chapter me pahuch gaye ho ziddi se pass mange ho aur derahahu kyuki tum vada kiye ho padnekeliye yar such batao tho exam me bhi tere bare me sochne laga hu , so teri vaja se nai lik paya 😠 ganji chudail tu magic karta he merepe kya samja ha muje mughal samja he kya hogwards ke bahar magic use nai karta hu 😤 nai thooo.... acha choti bachi he maf krdiya pata he tu uss yellow saree me bhut kubsurat ho ,lajawab, batsurat sorry bemisaal, gorgeous, poop sorry preety 😅, ladki tho achi ho par bauni ho koi na sambhal lunga. yar kabi dar lagta he ki kahi tum bhi na chod do yar usko tho beg nai kiya ki ruknekeliye par tereko beg kya tera pair bhi pakad sakta hu agar kabhi bf mila tho kam sekam bheje hue reels ka reply tho do atleast 2 reels enough tuje kon nai he mereko, bhad me jai sab log bas tu hi bolo vahi kafi he mujhe aur kya hi bolu tere bareme bolne keliye sahi sare he par ek din bhi kam he yar such bolu tho mene usko bhi itna attach aur pyar nai hua jitna tum se hua he kyuki she loves the way i loved her. ab ladkiyo me intrest tho kab ka ja chuka he par terepe nai nai nai ab tho pukka line mar rahahu ☠️ , vo sorry huh me nai mera hormones lik rahehe. kabhi tatti jaye tho taste kro achihogi tatse. tuje ek boquet gift karke ur so gorgeously dump bolna chanta hu par koi na ek din zarur karunga. sehj, maybe i cant be with u my whole life maybe i die, or u get gf, or any other but dont expect i get a gf and i ignore u usse breakup krlunga par tuje nai chodunga. ur more than a friend to me . ab zyada mat soch itna hi dimag he use krkiya tho exa me kya use krogi aur ek bath kabhi bhulna mat kuje please. jab successful hojaunga tab tu bhi mere sath hogi behind a every successful man there is a woman pur tu tho ladka he na acha koi na .i maybe not that close to you sehj but u r a candle in my life . zindagi ek room me ,gym me , class room ke last bench me guzara he lonelyness se par jab se tu ayi fir normal hgya hu . i really think now ki god ek reason se him hume milvaya he god must be crazy oar god ka choice itni achi he manna padega. chal bhuddi ab muje tho tarif kr tereliye paragraphs lik rahahu upar se boyfriend bhi nai hu kya timw he yr koi na sab kuch terekiye par tum na padlo achi marks ana chahiye thik he ? smj gaye ? agar tu boards pass krliya tho tere ko me bangles gift karunga deal? aur ek bhat muje SAM IS GOD  bhol tab kuch dikaunga its a surprise 😜 utna bhi special nai but jo bi ho pyar se kara hu aur ye sab me pyar se lik rahahu tho badtameez mat dika smj gayi acha fir chalta hu tere dil me 🤪 .",
-        password: "done", 
+        secretMessage: " you!, yes u heared it right i love you , respectfully.sehj meri jindagi me 2 hi ladkiya ayi he ab tak ek vo jis ke bare me bat nai karunga aur ek tum bat kare tho karta hi rahunga ab ye mat soch na ki line mar rahahu ,maybe oh nono not nai mar rahahu . tho last chapter me pahuch gaye ho ziddi se pass mange ho aur derahahu kyuki tum vada kiye ho padnekeliye yar such batao tho exam me bhi tere bare me sochne laga hu , so teri vaja se nai lik paya 😠 ganji chudail tu magic karta he merepe kya samja ha muje mughal samja he kya hogwards ke bahar magic use nai karta hu 😤 nai thooo.... acha choti bachi he maf krdiya pata he tu uss yellow saree me bhut kubsurat ho ,lajawab, batsurat sorry bemisaal, gorgeous, poop sorry preety 😅, ladki tho achi ho par bauni ho koi na sambhal lunga. yar kabi dar lagta he ki kahi tum bhi na chod do yar usko tho beg nai kiya ki ruknekeliye par tereko beg kya tera pair bhi pakad sakta hu agar kabhi bf mila tho kam sekam bheje hue reels ka reply tho do atleast 2 reels enough tuje kon nai he mereko, bhad me jai sab log bas tu hi bolo vahi kafi he mujhe aur kya hi bolu tere bareme bolne keliye sahi sare he par ek din bhi kam he yar such bolu tho mene usko bhi itna attach aur pyar nai hua jitna tum se hua he kyuki she loves the way i loved her. ab ladkiyo me intrest tho kab ka ja chuka he par terepe nai nai nai ab tho pukka line mar rahahu ☠️ , vo sorry huh me nai mera hormones lik rahehe. kabhi tatti jaye tho taste kro achihogi tatse. tuje ek boquet gift karke ur so gorgeously dump bolna chanta hu par koi na ek din zarur karunga. sehj, maybe i cant be with u my whole life maybe i die, or u get gf, or any other but dont expect i get a gf and i ignore u usse breakup krlunga par tuje nai chodunga. ur more than a friend to me . ab zyada mat soch itna hi dimag he use krkiya tho exa me kya use krogi aur ek bath kabhi bhulna mat kuje please. jab successful hojaunga tab tu bhi mere sath hogi behind a every successful man there is a woman pur tu tho ladka he na acha koi na .i maybe not that close to you sehj but u r a candle in my life . zindagi ek room me ,gym me , class room ke last bench me guzara he lonelyness se par jab se tu ayi fir normal hgya hu . i really think now ki god ek reason se him hume milvaya he god must be crazy oar god ka choice itni achi he manna padega. chal bhuddi ab muje tho tarif kr tereliye paragraphs lik rahahu upar se boyfriend bhi nai hu kya timw he yr koi na sab kuch terekiye par tum na padlo achi marks ana chahiye thik he ? smj gaye ? agar tu boards pass krliya tho tere ko me bangles gift karunga deal? aur ek bhat muje SAM IS GOD  bhol tab kuch dikaunga its a surprise 😜 utna bhi special nai but jo bi ho pyar se kara hu aur ye sab me pyar se lik rahahu tho badtameez mat dika smj gayi acha fir chalta hu tere dil me 🤪 ",
+        password: "samisfool", 
         locked: true,
         requiresAgreement: false,
         agreed: true
@@ -55,6 +55,7 @@ const wrapper = document.getElementById('cards-wrapper');
 const modalOverlay = document.getElementById('modal-overlay');
 const passwordInput = document.getElementById('password-input');
 const finalReward = document.getElementById('final-reward');
+const audioPlayer = document.getElementById('bg-music');
 let currentCardId = null;
 
 // Load state from local storage
@@ -64,7 +65,6 @@ if (savedState) {
     cardsData.forEach((card, index) => {
         if(savedState[index]) {
             card.locked = savedState[index].locked;
-            // Restore agreement status if it exists
             if (savedState[index].agreed !== undefined) {
                 card.agreed = savedState[index].agreed;
             }
@@ -80,7 +80,6 @@ function renderCards() {
         const cardEl = document.createElement('div');
         cardEl.className = `card ${card.locked ? '' : 'unlocked'}`;
         
-        // Only allow clicking to unlock if it is currently locked
         if (card.locked) {
             cardEl.onclick = () => handleCardClick(card.id);
         }
@@ -89,11 +88,10 @@ function renderCards() {
         const blurClass = card.locked ? 'locked-blur' : '';
         const textDisplay = card.locked ? 'This message is locked.' : card.secretMessage;
 
-        // LOGIC: If unlocked BUT agreement not met yet, show the button
         let contentHTML = '';
 
+        // 1. AGREEMENT PRANK LOGIC
         if (!card.locked && card.requiresAgreement && !card.agreed) {
-            // SHOW THE TRICK BUTTON
             contentHTML = `
                 <div class="agreement-wrapper">
                     <p class="agreement-label">Unlock content:</p>
@@ -102,11 +100,21 @@ function renderCards() {
                     </button>
                 </div>
             `;
-        } else {
-            // NORMAL CONTENT (Image + Text)
-            // If locked, image is blurred. If unlocked, image is clear.
+        } 
+        // 2. CHAPTER 4 MUSIC BUTTON (If she refreshes page, she can play it again)
+        else if (!card.locked && card.id === 4) {
+            contentHTML = `
+                <div class="card-content">
+                    ${textDisplay}
+                </div>
+                <button class="music-btn" onclick="playMusic()">
+                    🎵 Replay Music
+                </button>
+            `;
+        }
+        // 3. NORMAL CONTENT
+        else {
             const imageHTML = card.image ? `<img src="${card.image}" class="card-image ${blurClass}">` : '';
-            
             contentHTML = `
                 ${imageHTML}
                 <div class="card-content ${blurClass}">
@@ -149,6 +157,12 @@ function triggerAgreement(id) {
     renderCards();
 }
 
+function playMusic() {
+    audioPlayer.play().catch(error => {
+        console.log("Autoplay prevented by browser, user must interact.");
+    });
+}
+
 function closeModal() {
     modalOverlay.classList.remove('active');
     currentCardId = null;
@@ -163,6 +177,12 @@ function checkPassword() {
         saveState();
         renderCards();
         closeModal();
+
+        // 🎵 IF CHAPTER 4, PLAY MUSIC IMMEDIATELY
+        if (card.id === 4) {
+            playMusic();
+        }
+
     } else {
         const modalBox = document.getElementById('modal-box');
         modalBox.classList.add('shake');
